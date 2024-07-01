79ac9b156dc513ea5c4288dc5b2cbdd427dc41d7
Renan <renanlazoti@gmail.com>
2024-06-30 21:06:55 -0300

    feat: created card for social networks

---
+++ src/components/socialCard/SocialCard.jsx
@@ -0,0 +1,11 @@
+import { CardContainer } from "./SocialCard.style";
+
+const SocialCard = ({ icon: Icon}) => {
+  return (
+    <CardContainer>
+        <Icon />
+    </CardContainer>
+  )
+}
+
+export default SocialCard
---
+++ src/components/socialCard/SocialCard.style.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import { FaGithub } from "react-icons/fa";
+
+export const CardContainer = styled.div`
+    width: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding: .7rem;
+    border-bottom: .3rem solid var(--color-secondary);
+    border-radius: var(--border-radius2);
+
+    svg {
+        width: 2.5rem;
+        height: 2.5rem;
+    }
+
+    &:hover {
+        cursor: pointer;
+        transition: var(--transition-slow);
+        border-bottom: .3rem solid var(--color-primary);
+    }
+`
+
+export const CardIcon = styled(FaGithub)`
+
+`

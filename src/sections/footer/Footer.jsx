1859d318fc542672c0b88388fac86cb5ab9c2143
Renan <renanlazoti@gmail.com>
2024-07-15 21:06:16 -0300

    feat: created footer section

---
+++ src/sections/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import {FooterContainer} from './Footer.style'
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+        <p>© 2024 Renan Lazoti. Todos os direitos reservados.</p>
+        <a href='https://storyset.com/work' target='_blank'>Work illustrations by Storyset</a>
+    </FooterContainer>
+  )
+}
+
+export default Footer
---
+++ src/sections/footer/Footer.style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const FooterContainer = styled.footer`
+    padding: 1.5rem 0;
+    background-color: var(--color-primary);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    p, a {
+        color: var(--color-light);
+    }
+`
